762f3c9bca27df184d53a12f958a43eeba83a938
孟凡钧 <1437615458@qq.com>
2024-04-22 14:12:37 +0800

    Update SelectPic.py

--- SelectPic.py
+++ SelectPic.py
@@ -110,7 +110,7 @@ class FilePicker(QWidget):
 
     def TakePic(self):
 
-        subprocess.Popen(['python','TakePic.py'])
+        subprocess.Popen(['python3','TakePic.py'])
 
     # def showOutput(self):
     #     self.output_text_edit.appendPlainText("")
